5cd80fcfd35dd5bed7f41d09c11bcc8768be29b2
Ernesto Ramírez <ernestorb_@outlook.com>
2022-11-11 00:10:23 -0600

    Refactor to maven modules

---
+++ Plugin/src/main/java/com/ernestorb/tablistmanager/Main.java
@@ -0,0 +1,24 @@
+package com.ernestorb.tablistmanager;
+
+import com.ernestorb.tablistmanager.commands.TablistCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+
+    private TablistManager manager;
+
+    @Override
+    public void onEnable() {
+        this.manager = new TablistManager(this);
+        this.getCommand("tablist").setExecutor(new TablistCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    public TablistManager getManager() {
+        return manager;
+    }
+}
