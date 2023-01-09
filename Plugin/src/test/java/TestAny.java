af3a82b573f17fbd488f0d396d446d74289ce4ff
Ernesto Ramírez <ernestorb_@outlook.com>
2023-01-08 22:31:31 -0600

    Add some fake tests to include JUnit.

---
+++ Plugin/src/test/java/TestAny.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestAny {
+
+    @Test
+    public void addition() {
+        assertEquals(5,5);
+    }
+
+}
